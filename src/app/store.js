109daec95b5a42a7a3ec1e2b01facff4ad33a2f5
Yago <yagomateos@hotmail.com>
2023-03-16 16:24:55 +0100

    updated

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
-
 import { cryptoApi } from '../services/cryptoApi';
 import { cryptoNewsApi } from '../services/cryptoNewsApi';
+import { configureStore } from '@reduxjs/toolkit';
+
+// Tu código aquí
 
 export default configureStore({
-  reducer: {
-    [cryptoApi.reducerPath]: cryptoApi.reducer,
-    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
-  },
+reducer: {
+[cryptoApi.reducerPath]: cryptoApi.reducer,
+[cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
+},
+middleware: (getDefaultMiddleware) =>
+getDefaultMiddleware().concat(
+cryptoApi.middleware,
+cryptoNewsApi.middleware,
+),
 });
